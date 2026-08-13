9834e37598a5bb38bfd7b9c8237f50fb280e44e5
NeelContractor <neelcontractor7@gmail.com>
2026-08-13 16:54:23 +0530

    corrected openrouter svg code

--- src/components/svgs/openrouter.tsx
+++ src/components/svgs/openrouter.tsx
@@ -1,15 +1,20 @@
-export const OpenRouter = ({ className }: { className: string }) => {
-    return (
-      <svg
-        className={className}
-        viewBox="0 0 24 24"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M6 3v13.5a3 3 0 1 0 2 2.828V19a5 5 0 0 1 5-5h1.586l-2.293-2.293 1.414-1.414L17 14l-4.293 4.293-1.414-1.414L13.586 15H13a3 3 0 0 0-3 3h2a1 1 0 0 1 1 1v.5a3 3 0 1 0 2-2.828V17a7 7 0 0 0-7-7H6v-7z"
-          fill="currentColor"
-        />
-      </svg>
-    );
-  };
+export const OpenRouter = ({ className }: { className?: string }) => {
+  return (
+    <svg
+      className={className}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      aria-label="OpenRouter"
+    >
+      <path
+        d="M18.5 5.5H8.41l2.3-2.3L9.29 1.79 4.5 6.58l4.79 4.79 1.42-1.41-2.3-2.3H18.5a2 2 0 0 1 2 2v1.5h2V9.66a4.16 4.16 0 0 0-4-4.16Z"
+        fill="currentColor"
+      />
+      <path
+        d="M5.5 18.5h10.09l-2.3 2.3 1.42 1.41 4.79-4.79-4.79-4.79-1.42 1.41 2.3 2.3H5.5a2 2 0 0 1-2-2v-1.5h-2v1.5a4 4 0 0 0 4 4.16Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+};
